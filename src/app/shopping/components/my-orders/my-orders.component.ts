1cc277b54c209e7ddc44ade731a7b2eb9753391d
Adam <adamski01231@gmail.com>
2017-12-10 15:09:02 +0100

    Improved project file structure and modularization.

---
+++ src/app/shopping/components/my-orders/my-orders.component.ts
@@ -0,0 +1,20 @@
+import { AuthService } from '../../../shared/services/auth.service';
+import { OrderService } from '../../../shared/services/order.service';
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-orders',
+  templateUrl: './my-orders.component.html',
+  styleUrls: ['./my-orders.component.css']
+})
+export class MyOrdersComponent {
+  orders$;
+
+  constructor(
+    private authService: AuthService,
+    private orderService: OrderService) {
+
+    this.orders$ = authService.user$.switchMap(u => orderService.getOrdersByUser(u.uid));
+  }
+
+}
